b53d5bcd36bcdbf8ec6a497a58fa1c14dad5958c
eric schkufza <eric.schkufza@gmail.com>
2016-06-26 15:15:58 -0700

    Added meta submodule. Generalized DirArg to associative and sequence containers.

--- include/dir_arg.h
+++ include/dir_arg.h
@@ -4,46 +4,60 @@
 #include <dirent.h>
 #include <sstream>
 #include <sys/stat.h>
+#include <type_traits>
+#include "ext/meta/include/is_stl_associative.h"
+#include "ext/meta/include/is_stl_sequence.h"
 #include "include/file_arg.h"
 
 namespace cl {
 
 template <typename T, typename R>
-struct DirReader {
-  bool operator()(std::istream& is, T& t) const {
-    std::string path = "";
-    is >> path;
+class DirReader {
+  public:
+    bool operator()(std::istream& is, T& t) const {
+      std::string path = "";
+      is >> path;
 
-    DIR* dp = opendir(path.c_str());    
-    if (dp == NULL) {
-      return false;
-    }
-    while (dirent* de = readdir(dp)) {
-      const std::string file = de->d_name;
-      if (file == "." || file == "..") {
-        continue;
-      }
-      std::stringstream ss(path + "/" + file);
-
-      struct stat filestat;
-      if (stat(ss.str().c_str(), &filestat)) {
+      DIR* dp = opendir(path.c_str());    
+      if (dp == NULL) {
         return false;
-      } 
-      if (S_ISDIR(filestat.st_mode)) {
-        if (!DirReader<T,R>()(ss, t)) {
+      }
+      while (dirent* de = readdir(dp)) {
+        const std::string file = de->d_name;
+        if (file == "." || file == "..") {
+          continue;
+        }
+        std::stringstream ss(path + "/" + file);
+
+        struct stat filestat;
+        if (stat(ss.str().c_str(), &filestat)) {
+          return false;
+        } 
+        if (S_ISDIR(filestat.st_mode)) {
+          if (!DirReader<T,R>()(ss, t)) {
+            return false;
+          }
+          continue;
+        }
+        typename T::value_type v;
+        if (!FileReader<typename T::value_type,R>()(ss, v)) {
           return false;
         }
-        continue;
+        add(t, v);
       }
-      typename T::value_type v;
-      if (!FileReader<typename T::value_type,R>()(ss, v)) {
-        return false;
-      }
-      t.push_back(v);
+
+      return true;
     }
 
-    return true;
-  }
+  private:
+    template <typename C = T>
+    void add(typename std::enable_if<meta::is_stl_associative<C>::value, C>::type& c, const typename C::value_type& v) const {
+      c.insert(v);
+    }
+    template <typename C = T>
+    void add(typename std::enable_if<meta::is_stl_sequence<C>::value, C>::type& c, const typename C::value_type& v) const {
+      c.push_back(v);
+    }
 };
 
 template <typename T, typename R = ValReader<typename T::value_type>, typename W = ValWriter<T>>
