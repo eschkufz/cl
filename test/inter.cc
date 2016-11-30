d973f6f661e1524764d47349900629e515990bf7
eric schkufza <eric.schkufza@gmail.com>
2016-11-30 15:13:16 -0800

    fixes issue 6

--- test/inter.cc
+++ test/inter.cc
@@ -6,7 +6,9 @@ using namespace std;
 
 // Check the visibility of args defined in other translation units
 TEST(inter, visible) {
-  const auto itr = Args::arg_find("--global");
-  EXPECT_NE(itr, Args::arg_end());
+  const auto f = [](Arg* a) {
+    return std::find(a->alias_begin(), a->alias_end(), "--global") != a->alias_end();
+  };
+  EXPECT_NE(find_if(Args::arg_begin(), Args::arg_end(), f), Args::arg_end());
 }
 
